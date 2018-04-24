a647ead007117ebe7df5fa325f338c003b2ed6ea
RohitBagda <rbagda@macalester.edu>
2018-04-23 20:55:16 -0500

    refactored transitionToCombo

--- states/js/transitionToCombo.js
+++ states/js/transitionToCombo.js
@@ -4,8 +4,8 @@ var transitionToComboState = {
 
         game.add.sprite(0,0, "background");
 
-        this.showFrenzyModeAnimation();
-        // this.game.state.start('frenzy');
+        this.showcomboModeAnimation();
+        // this.game.state.start('combo');
 
         game.time.events.loop(300, function(){
             // Console.log(this.currentTime);
@@ -26,15 +26,15 @@ var transitionToComboState = {
 
     },
 
-    showFrenzyModeAnimation: function(){
+    showcomboModeAnimation: function(){
 
         //var textSize = canvasWidth + "px";
-        var frenzyTextFormat = {font: "bold 200px Times", fill: "#00FF00"};
-        frenzyTextFormat.stroke = "#000000";
-        frenzyTextFormat.strokeThickness = 5;
-        var frenzyText = "COMBO";
+        var comboTextFormat = {font: "bold 200px Times", fill: "#00FF00"};
+        comboTextFormat.stroke = "#000000";
+        comboTextFormat.strokeThickness = 5;
+        var comboText = "COMBO";
 
-        var horizontalOffSet = this.calculateOffSet(frenzyText);
+        var horizontalOffSet = this.calculateOffSet(comboText);
         var verticalOffset = 70;
         console.log("screenwidth: " + canvasWidth);
         console.log("screen center: " + game.world.centerX);
@@ -42,10 +42,10 @@ var transitionToComboState = {
         console.log("text Position: " + (game.world.centerX - horizontalOffSet));
 
 
-        this.frenzyTextDisplay = this.game.add.text(canvasWidth/2, canvasHeight/2, frenzyText, frenzyTextFormat);
-        this.frenzyTextDisplay.anchor.setTo(0.5, 0.5);
-        this.frenzyTextDisplay.align = 'center';
-        this.game.add.tween(this.frenzyTextDisplay)
+        this.comboTextDisplay = this.game.add.text(canvasWidth/2, canvasHeight/2, comboText, comboTextFormat);
+        this.comboTextDisplay.anchor.setTo(0.5, 0.5);
+        this.comboTextDisplay.align = 'center';
+        this.game.add.tween(this.comboTextDisplay)
             .to({alpha: 0}, 100, Phaser.Easing.Default, true, 500)
             .onComplete.add(function () {
                 console.log("This is called when the tween is done.");
