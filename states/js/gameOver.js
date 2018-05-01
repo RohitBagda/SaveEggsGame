2d6157842d9ed6be796550fb4ae3c483147f0dd1
Katya <ykelly@macalester.edu>
2018-05-01 16:11:30 -0500

    reformatted gameOver state and made music play after pressing play again

--- states/js/gameOver.js
+++ states/js/gameOver.js
@@ -2,13 +2,21 @@ var gameOverState = {
     create: function () {
         game.add.sprite(0,0, "background");
         var scoreLabel = this.game.add.text(canvasWidth/3.5, canvasHeight/3, "Your score is " + score,
-            {fontSize: '50px', fill: "#ff0000"});
+            {font: 'bold 50px Corbel', fill: "#ff0000"});
 
         var highestScoreLabel = this.game.add.text(canvasWidth/3.5, canvasHeight/3 + 100, "Your Highest Score is " + highestScore,
-            {fontSize: '48px', fill: "#ff00ff"});
+            {font: 'bold 48px Corbel', fill: "#ff00ff"});
 
-        var playButton = game.add.button(canvasWidth/2, canvasHeight/2,"play",this.restart, this);
-        playButton.anchor.setTo(0.5, 0.5);
+        var playAgainButtonFormatting = {font: "bold 70pt Corbel", fill: "#003366"};
+        playAgainButtonFormatting.stroke = "#000000";
+        playAgainButtonFormatting.strokeThickness = 1;
+
+        var verticalSpaceBetweenButtons = 140;
+
+        var playAgainButton = this.game.add.text(canvasWidth/2, canvasHeight/2 + verticalSpaceBetweenButtons, "Play Again", playAgainButtonFormatting);
+        playAgainButton.anchor.setTo(0.5, 0.5);
+        playAgainButton.inputEnabled = true;
+        playAgainButton.events.onInputDown.add(this.restart, this);
 
         score = 0;
         //this.game.input.onDown.addOnce(this.restart, this);
@@ -17,6 +25,7 @@ var gameOverState = {
     restart: function(){
         this.reset();
         this.game.state.start('play');
+        backgroundMusic.play();
     },
     
     reset: function () {
