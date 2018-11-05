3ee8eb1826064fa8ba2cb2ed593239633c663cf1
Roland Munsil <roland@munsil.com>
2018-11-05 17:33:40 -0600

    Fix velocity system in combo

--- www/js/combo.js
+++ www/js/combo.js
@@ -59,8 +59,6 @@ var comboState = {
         gameController.updateRainbowScoreColor();
         gameController.updateBasketPosition();
         for(var comboEgg of this.comboEggs.children){
-            // For each egg in the combo state. Set the initial velocity of the eggs
-            comboEgg.body.velocity.y=gameController.eggVelocity;
 
             // Check for collision between combo egg and basket.
             if(game.physics.arcade.overlap(gameController.player, comboEgg)) {
@@ -132,8 +130,8 @@ var comboState = {
             var egg = game.add.sprite(eggX, eggY, eggType);
             egg.scale.setTo(scaleRatio, scaleRatio);
             game.physics.arcade.enable(egg);
-            this.eggGravity = gameController.calculateEggGravity(gameController.elapsedEggFallingTimeSecs);
-            egg.body.gravity.y = this.eggGravity;
+            const eggGravity = gameController.calculateEggGravity(gameController.elapsedEggFallingTimeSecs);
+            egg.body.velocity.y = gameController.eggVelocity + (eggGravity * game.time.physicsElapsed);
             this.comboEggs.add(egg);
         }
     },
